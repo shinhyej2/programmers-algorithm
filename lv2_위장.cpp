054231ef2d62eb04cd662a7040f568a599d56ae2
shinhyej2 <shinhyej2@nate.com>
2022-05-15 16:50:15 +0900

    lv2_위장.cpp

--- lv2_위장.cpp
+++ lv2_위장.cpp
@@ -6,18 +6,15 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
 
-    //hasmap 만든다
     int answer = 1;
 
     unordered_map<string, int> map;
 
-    //map 에 넣기
     for (auto cloth : clothes)
     {
         map[cloth[1]] += 1;
     }
 
-    //map의 value 값으로 계산
     for (auto it = map.begin(); it != map.end(); it++)
     {
         answer *= it->second + 1;
