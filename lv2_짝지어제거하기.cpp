54ac9a8a03dd73114dcc5d31d9bffd6b507ae266
shinhyej2 <shinhyej2@nate.com>
2022-10-30 23:15:17 +0900

    lv2_짝지어제거하기.cpp

---
+++ lv2_짝지어제거하기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> str;    
+    for (int i = 0; i < s.length(); i++) 
+    {        
+        if (str.empty() || str.top() != s[i])
+        {
+            str.push(s[i]);
+        }
+        
+        else if (str.top() == s[i])
+        {
+            str.pop();
+        }
+    }
+    
+    if (str.empty())
+    {
+        return 1;
+    }
+
+    return 0;
+}
